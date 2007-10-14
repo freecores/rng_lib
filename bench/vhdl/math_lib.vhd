84299d139ff7526ed376f92398753177490ec5bb
gedra <gedra@41e526f7-2aaa-484d-ba2e-b59cbf60a2f5>
2007-10-14 13:35:04 +0000

    Code beautification

--- bench/vhdl/math_lib.vhd
+++ bench/vhdl/math_lib.vhd
@@ -44,186 +44,189 @@
 -- CVS Revision History
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.1  2004/09/28 15:02:56  gedra
+-- Math functions library.
 --
 --
- 
+--
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package math_lib is
 
-  function sqrt(x: real) return real;   -- returns square root
-  function ln(x: real ) return real;    -- natural logarithm
-  function log(x: real) return real;    -- base 10 logarithm
-  function exp(x: real ) return real;   -- exponential function
-    	
-  -- Some mathematical constants
-  constant MATH_E: real := 2.71828_18284_59045_23536;  
-  
+   function sqrt(x : real) return real;  -- returns square root
+   function ln(x   : real) return real;  -- natural logarithm
+   function log(x  : real) return real;  -- base 10 logarithm
+   function exp(x  : real) return real;  -- exponential function
+
+   -- Some mathematical constants
+   constant MATH_E : real := 2.71828_18284_59045_23536;
+   
 end math_lib;
 
 package body math_lib is
 
 -- Square root calculation
-  function sqrt (x : real ) return real is
-    -- returns square root of X;  X >= 0
-    --
-    -- Computes square root using the Newton-Raphson approximation:
-    -- F(n+1) = 0.5*[F(n) + x/F(n)];
-    --
-    
-    constant inival: real := 1.5;
-    constant eps : real := 0.000001;
-    constant relative_err : real := eps*X;
-    
-    variable oldval : real;
-    variable newval : real;
+   function sqrt (x : real) return real is
+      -- returns square root of X;  X >= 0
+      --
+      -- Computes square root using the Newton-Raphson approximation:
+      -- F(n+1) = 0.5*[F(n) + x/F(n)];
+      --
+      
+      constant inival       : real := 1.5;
+      constant eps          : real := 0.000001;
+      constant relative_err : real := eps*X;
 
-  begin
-    -- check validity of argument
-    if x < 0.0 then
-      report "x < 0 in sqrt(x)" 
-        severity failure;
-      return (0.0);
-    end if;
-    
-    -- get the square root for special cases
-    if x = 0.0 then
-      return 0.0;
-    else
-      if x = 1.0 then
-        return 1.0; -- return exact value
+      variable oldval : real;
+      variable newval : real;
+
+   begin
+      -- check validity of argument
+      if x < 0.0 then
+         report "x < 0 in sqrt(x)"
+            severity failure;
+         return (0.0);
       end if;
-    end if;
-    
-    -- get the square root for general cases
-    oldval := inival;
-    newval := (X/oldval + oldval)/2.0;
-    
-    while ( abs(newval -oldval) > relative_err ) loop
-      oldval := newval;
+
+      -- get the square root for special cases
+      if x = 0.0 then
+         return 0.0;
+      else
+         if x = 1.0 then
+            return 1.0;                 -- return exact value
+         end if;
+      end if;
+
+      -- get the square root for general cases
+      oldval := inival;
       newval := (X/oldval + oldval)/2.0;
-    end loop;
-    
-    return newval;
-  end sqrt;
+
+      while (abs(newval -oldval) > relative_err) loop
+         oldval := newval;
+         newval := (X/oldval + oldval)/2.0;
+      end loop;
+
+      return newval;
+   end sqrt;
 
 -- Natural logarithm calculation
-  function ln (x: real ) return real is
-    -- returns natural logarithm of X; X > 0
-    --
-    -- This function computes the exponential using the following series:
-    --    log(x) = 2[ (x-1)/(x+1) + (((x-1)/(x+1))**3)/3.0 + ...] ; x > 0
-    --
-    	
-    constant eps : real := 0.000001;	-- precision criteria
+   function ln (x : real) return real is
+      -- returns natural logarithm of X; X > 0
+      --
+      -- This function computes the exponential using the following series:
+      --    log(x) = 2[ (x-1)/(x+1) + (((x-1)/(x+1))**3)/3.0 + ...] ; x > 0
+      --
+      
+      constant eps : real := 0.000001;  -- precision criteria
 
-    variable xlocal: real ;			-- following variables are
-    variable oldval: real ;			-- used to evaluate the series
-    variable xlocalsqr: real ;
-    variable factor : real ;
-    variable count: integer ;
-    variable newval: real ;
-    
-  begin
-    -- check validity of argument
-    if x <= 0.0 then
-      report "x <= 0 in ln(x)" 
-        severity failure;
-      return(real'LOW);
-    end if;
-
-    -- compute value for special cases
-    if x = 1.0 then
-      return 0.0;
-    else
-      if x = MATH_E then
-        return 1.0;
+      variable xlocal    : real;        -- following variables are
+      variable oldval    : real;        -- used to evaluate the series
+      variable xlocalsqr : real;
+      variable factor    : real;
+      variable count     : integer;
+      variable newval    : real;
+      
+   begin
+      -- check validity of argument
+      if x <= 0.0 then
+         report "x <= 0 in ln(x)"
+            severity failure;
+         return(real'low);
       end if;
-    end if;
 
-    -- compute value for general cases
-    xlocal := (x - 1.0)/(x + 1.0);
-    oldval := xlocal;
-    xlocalsqr := xlocal*xlocal;
-    factor := xlocal*xlocalsqr; 
-    count := 3;
-    newval := oldval + (factor/real(count));
-    
-    while ( abs(newval - oldval) > eps ) loop
-      oldval := newval;
-      count := count +2;
-      factor := factor * xlocalsqr;
-      newval := oldval + factor/real(count);
-    end loop;
-    
-    newval := newval * 2.0;
-    return newval;
-  end ln;
+      -- compute value for special cases
+      if x = 1.0 then
+         return 0.0;
+      else
+         if x = MATH_E then
+            return 1.0;
+         end if;
+      end if;
+
+      -- compute value for general cases
+      xlocal    := (x - 1.0)/(x + 1.0);
+      oldval    := xlocal;
+      xlocalsqr := xlocal*xlocal;
+      factor    := xlocal*xlocalsqr;
+      count     := 3;
+      newval    := oldval + (factor/real(count));
+
+      while (abs(newval - oldval) > eps) loop
+         oldval := newval;
+         count  := count +2;
+         factor := factor * xlocalsqr;
+         newval := oldval + factor/real(count);
+      end loop;
+
+      newval := newval * 2.0;
+      return newval;
+   end ln;
 
 -- Base 10 logarithm calculation
-  function log (x: real) return real is
-    -- returns logarithm base 10 of x; x > 0
-  begin
-    -- check validity of argument
-    if x <= 0.0 then
-      assert false report "x <= 0.0 in log(x)" 
-        severity ERROR;
-      return(real'LOW);
-    end if;
+   function log (x : real) return real is
+      -- returns logarithm base 10 of x; x > 0
+   begin
+      -- check validity of argument
+      if x <= 0.0 then
+         assert false report "x <= 0.0 in log(x)"
+            severity error;
+         return(real'low);
+      end if;
 
-    -- compute the value
-    return (ln(x)/2.30258509299);
-  end log;
+      -- compute the value
+      return (ln(x)/2.30258509299);
+   end log;
 
 -- Calculate e**x
-  function exp (x: real ) return real is
-    -- returns e**X; where e = MATH_E
-    --
-    -- This function computes the exponential using the following series:
-    --    exp(x) = 1 + x + x**2/2! + x**3/3! + ... ; x > 0
-    --
-    constant eps : real := 0.000001;	-- precision criteria
-    
-    variable reciprocal: boolean := x < 0.0;-- check sign of argument
-    variable xlocal : real := abs(x);       -- use positive value
-    variable oldval: real ;			-- following variables are
-    variable num: real ;			-- used for series evaluation
-    variable count: integer ;
-    variable denom: real ;
-    variable newval: real ;
-    
-  begin
-    -- compute value for special cases
-    if x = 0.0 then
-      return 1.0;
-    else
-      if  x = 1.0  then
-        return MATH_E;
-      end if;
-    end if;
+   function exp (x : real) return real is
+      -- returns e**X; where e = MATH_E
+      --
+      -- This function computes the exponential using the following series:
+      --    exp(x) = 1 + x + x**2/2! + x**3/3! + ... ; x > 0
+      --
+      constant eps : real := 0.000001;  -- precision criteria
 
-    -- compute value for general cases
-    oldval := 1.0;
-    num := xlocal;
-    count := 1;
-    denom := 1.0;
-    newval:= oldval + num/denom;
-    
-    while ( abs(newval - oldval) > eps ) loop
-      oldval := newval;
-      num := num*xlocal;
-      count := count +1;
-      denom := denom*(real(count));
+      variable reciprocal : boolean := x < 0.0;  -- check sign of argument
+      variable xlocal     : real    := abs(x);   -- use positive value
+      variable oldval     : real;                -- following variables are
+      variable num        : real;                -- used for series evaluation
+      variable count      : integer;
+      variable denom      : real;
+      variable newval     : real;
+      
+   begin
+      -- compute value for special cases
+      if x = 0.0 then
+         return 1.0;
+      else
+         if x = 1.0 then
+            return MATH_E;
+         end if;
+      end if;
+
+      -- compute value for general cases
+      oldval := 1.0;
+      num    := xlocal;
+      count  := 1;
+      denom  := 1.0;
       newval := oldval + num/denom;
-    end loop;
-    
-    if reciprocal then
-      newval := 1.0/newval;
-    end if;
-    
-    return newval;
-  end exp;
+
+      while (abs(newval - oldval) > eps) loop
+         oldval := newval;
+         num    := num*xlocal;
+         count  := count +1;
+         denom  := denom*(real(count));
+         newval := oldval + num/denom;
+      end loop;
+
+      if reciprocal then
+         newval := 1.0/newval;
+      end if;
+
+      return newval;
+   end exp;
 
 end math_lib;
--- bench/vhdl/tb_rng.vhd
+++ bench/vhdl/tb_rng.vhd
@@ -44,112 +44,115 @@
 -- CVS Revision History
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.1  2004/09/28 15:12:52  gedra
+-- Test bench for random numbers.
 --
 --
- 
+--
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.textio.all;
 use work.rng_lib.all;
 
-entity tb_rng is  
+entity tb_rng is
 
 end tb_rng;
 
 architecture behav of tb_rng is
 
-  constant BIN_COUNT : integer := 15;
-  constant PLOT_HEIGHT : real := 40.0;
-  constant RAND_COUNT : integer := 10000;
-  
-  type rand_array is array (0 to RAND_COUNT - 1) of real; -- array used for plot
-  type bin_array is array (0 to BIN_COUNT - 1) of integer;
+   constant BIN_COUNT   : integer := 15;
+   constant PLOT_HEIGHT : real    := 40.0;
+   constant RAND_COUNT  : integer := 10000;
+
+   type rand_array is array (0 to RAND_COUNT - 1) of real;  -- array used for plot
+   type bin_array is array (0 to BIN_COUNT - 1) of integer;
 
 -- Plot a distribution of the numbers that are between lo,hi values
-  impure function plot_dist (numbs: rand_array; lo, hi: real) return integer is 
-    variable bins: bin_array;
-    variable bin_size, height : real;
-    variable idx, max: integer;
-    variable bar : line;
-  begin
-    -- reset bins
-    for i in 0 to BIN_COUNT - 1 loop
-      bins(i) := 0;
-    end loop;
-    -- sort numbers into bins
-    bin_size := (hi - lo) / real(BIN_COUNT);
-    for i in 0 to RAND_COUNT - 1 loop
-      if numbs(i) > lo and numbs(i) < hi then
-        idx := integer(((numbs(i) - lo) / bin_size) - 0.5);
-        if idx > BIN_COUNT - 1 then
-          idx := BIN_COUNT - 1;
-        elsif idx < 0 then
-          idx := 0;
-        end if;
-        bins(idx) := bins(idx) + 1;
-      end if;
-    end loop;
-    -- find largest bin
-    max := 0;
-    for i in 0 to BIN_COUNT - 1 loop
-      if bins(i) > max then
-        max := bins(i);
-      end if;
-    end loop;
-    -- plot bins
-    for i in 0 to BIN_COUNT - 1 loop
-      height := PLOT_HEIGHT * real(bins(i)) / real(max);
-      for j in 1 to integer(height) loop
-        write(bar, string'("*"));
+   impure function plot_dist (numbs : rand_array; lo, hi : real) return integer is
+      variable bins             : bin_array;
+      variable bin_size, height : real;
+      variable idx, max         : integer;
+      variable bar              : line;
+   begin
+      -- reset bins
+      for i in 0 to BIN_COUNT - 1 loop
+         bins(i) := 0;
       end loop;
-      writeline(OUTPUT, bar);
-    end loop;  
-    return 0;
-  end plot_dist;
-  
+      -- sort numbers into bins
+      bin_size := (hi - lo) / real(BIN_COUNT);
+      for i in 0 to RAND_COUNT - 1 loop
+         if numbs(i) > lo and numbs(i) < hi then
+            idx := integer(((numbs(i) - lo) / bin_size) - 0.5);
+            if idx > BIN_COUNT - 1 then
+               idx := BIN_COUNT - 1;
+            elsif idx < 0 then
+               idx := 0;
+            end if;
+            bins(idx) := bins(idx) + 1;
+         end if;
+      end loop;
+      -- find largest bin
+      max := 0;
+      for i in 0 to BIN_COUNT - 1 loop
+         if bins(i) > max then
+            max := bins(i);
+         end if;
+      end loop;
+      -- plot bins
+      for i in 0 to BIN_COUNT - 1 loop
+         height := PLOT_HEIGHT * real(bins(i)) / real(max);
+         for j in 1 to integer(height) loop
+            write(bar, string'("*"));
+         end loop;
+         writeline(OUTPUT, bar);
+      end loop;
+      return 0;
+   end plot_dist;
+   
 begin
 
-  p1: process
-    variable r_uni, r_gauss, r_exp : rand_var;
-    variable r_poisson : rand_var;
-    variable txt : line;
-    variable a: integer;
-    variable numbs : rand_array;
-  begin
-    -- Test the uniform distribution
-    r_uni := init_uniform(0, 0, 0, 0.0, 10.0);  -- range 0 to 10
-    t1: for i in 0 to RAND_COUNT - 1 loop
-      r_uni := rand(r_uni);
-      numbs(i) := r_uni.rnd;
-    end loop t1;
-    write(txt, string'("Uniform distribution:"));
-    writeline(OUTPUT, txt);
-    a := plot_dist (numbs, 0.0, 10.0);
-    -- Test the gaussian distribution
-    r_gauss := init_gaussian(0, 0, 0, 0.0, 10.0);  -- mean=0, stdev=10
-    t2: for i in 0 to RAND_COUNT - 1 loop
-      r_gauss := rand(r_gauss);
-      numbs(i) := r_gauss.rnd;
-    end loop t2;
-    write(txt, string'("Gaussian distribution:"));
-    writeline(OUTPUT, txt);
-    a := plot_dist (numbs, -20.0, 20.0);
-    -- Test the exponential distribution
-    r_exp := init_exponential(0, 0, 0, 10.0);  -- mean=10
-    t3: for i in 0 to RAND_COUNT - 1 loop
-      r_exp := rand(r_exp);
-      numbs(i) := r_exp.rnd;
-    end loop t3;
-    write(txt, string'("Exponential distribution:"));
-    writeline(OUTPUT, txt);
-    a := plot_dist (numbs, 0.0, 20.0);
-    
-    wait for 1 ns;
-    
-    report "End of simulation! (ignore this failure)"
-      severity failure;
-    wait;
-  end process p1;
-  
+   p1 : process
+      variable r_uni, r_gauss, r_exp : rand_var;
+      variable r_poisson             : rand_var;
+      variable txt                   : line;
+      variable a                     : integer;
+      variable numbs                 : rand_array;
+   begin
+      -- Test the uniform distribution
+      r_uni := init_uniform(0, 0, 0, 0.0, 10.0);     -- range 0 to 10
+      t1 : for i in 0 to RAND_COUNT - 1 loop
+         r_uni    := rand(r_uni);
+         numbs(i) := r_uni.rnd;
+      end loop t1;
+      write(txt, string'("Uniform distribution:"));
+      writeline(OUTPUT, txt);
+      a       := plot_dist (numbs, 0.0, 10.0);
+      -- Test the gaussian distribution
+      r_gauss := init_gaussian(0, 0, 0, 0.0, 10.0);  -- mean=0, stdev=10
+      t2 : for i in 0 to RAND_COUNT - 1 loop
+         r_gauss  := rand(r_gauss);
+         numbs(i) := r_gauss.rnd;
+      end loop t2;
+      write(txt, string'("Gaussian distribution:"));
+      writeline(OUTPUT, txt);
+      a     := plot_dist (numbs, -20.0, 20.0);
+      -- Test the exponential distribution
+      r_exp := init_exponential(0, 0, 0, 10.0);      -- mean=10
+      t3 : for i in 0 to RAND_COUNT - 1 loop
+         r_exp    := rand(r_exp);
+         numbs(i) := r_exp.rnd;
+      end loop t3;
+      write(txt, string'("Exponential distribution:"));
+      writeline(OUTPUT, txt);
+      a := plot_dist (numbs, 0.0, 20.0);
+
+      wait for 1 ns;
+
+      report "End of simulation! (ignore this failure)"
+         severity failure;
+      wait;
+   end process p1;
+   
 end behav;
